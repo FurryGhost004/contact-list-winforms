3a55f3470c89b4f162c938cd30ecd69cfe919394
FurryGhost004 <vothaithinh004@gmail.com>
2025-03-30 14:11:03 +0700

    Revert "êm Form thêm liên hệ từ Thành viên B"

--- ContactList/MainForm.Designer.cs
+++ ContactList/MainForm.Designer.cs
@@ -45,11 +45,10 @@
             // 
             this.button1.Location = new System.Drawing.Point(106, 116);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Thêm liên hệ";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
